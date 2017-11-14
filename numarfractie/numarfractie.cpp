79f7c0b14a0c6b603370859fdd16bc55726ce303
zegline <eugenzdrincu7@gmail.com>
2017-11-14 19:01:10 +0200

    added numarfractie

---
+++ numarfractie/numarfractie.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+    double numarDat;
+    int a = 0;
+    int b = 0;
+
+int main(){
+
+    cout << "Dati un numar : " << endl;
+    cin >> nuamrDat;
+
+    
+
+    cout << "a este " << a << " iar b este " << b << endl;
+
+    return 0;
+}
