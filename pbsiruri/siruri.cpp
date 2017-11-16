0ce22b65a49bbff4cf71f6f3fa7d7455afa82d71
zegline <eugenzdrincu7@gmail.com>
2017-11-16 14:20:41 +0200

    problem1

--- pbsiruri/siruri.cpp
+++ pbsiruri/siruri.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string.h>
+
+//definim limita de caractere pentru sirurile date in probleme
+#define LIMIT_CHAR 256
+
 using namespace std;
 
 int main(){
 
+    char char1[LIMIT_CHAR];
+    cin.get(char1, 255);
+    for (int i = 0; i < strlen(char1)/2; i++){
+        char1[i] = ' ';
+        char1[strlen(char1)-i] = ' ';
+        cout << char1 << endl;
+    }
 }
