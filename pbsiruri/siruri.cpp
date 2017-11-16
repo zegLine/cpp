7e580e0612be13d404495210c7077c8195c15f16
zegline <eugenzdrincu7@gmail.com>
2017-11-16 14:28:16 +0200

    problemele 2 si 3

--- pbsiruri/siruri.cpp
+++ pbsiruri/siruri.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-int main(){
+void problema1(){
 
     char char1[LIMIT_CHAR];
     cin.get(char1, 255);
@@ -15,4 +15,40 @@ int main(){
         char1[strlen(char1)-i] = ' ';
         cout << char1 << endl;
     }
+
+}
+
+void problema2(){
+
+    int i = 0;
+    int j = 0;
+    char char2[LIMIT_CHAR];
+    char vocale = 'aeiouAEIOU';
+    cin.get(char2, 255);
+    for(i=0;i<strlen(char2);i++)
+        if (strchr(vocale,char2[i])!=0)
+            {
+                j++;
+                strcpy(char2+i,char2+i+1);
+            }
+    cout<<j<<" "<<char2;
+    
+}
+
+void problema3(){
+
+    char vocale[]="aeiou";
+    char char3[40];
+    int i;
+    cin>>s;
+    for(i=0;i<strlen(s);i++)
+    if(strchr(vocale,char3[i])!=0) {
+        char3[i]=char3[i]+'A'-'a';
+    }
+    cout<<char3;
+
+}
+
+int main(){
+
 }
