0684dc7cd81458294dc062916ca74dba81bce3f7
zegline <eugenzdrincu7@gmail.com>
2017-11-16 14:33:18 +0200

    prob 4 si informatii comentarii

--- pbsiruri/siruri.cpp
+++ pbsiruri/siruri.cpp
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+/*
+Se citeste un cuvant format din litere mici.
+Sa se afiseze cuvintele care se obtin prin eliminarea succesiva
+a primei si ultimei litere din cuvantul citit.
+*/
+
 void problema1(){
 
     char char1[LIMIT_CHAR];
@@ -17,6 +23,11 @@ void problema1(){
     }
 
 }
+
+/*
+Se citeste un sir de caractere format din cel mult 100 de caractere.
+Sa se numere si sa se stearga vocalele din sir. 
+*/
 
 void problema2(){
 
@@ -35,6 +46,11 @@ void problema2(){
     
 }
 
+/*
+Se citeste un cuvant format din litere mici.
+Sa se inlocuiasca fiecare vocala mica din cuvant cu vocala mare corespunzatoare.
+*/
+
 void problema3(){
 
     char vocale[]="aeiou";
@@ -46,6 +62,27 @@ void problema3(){
         char3[i]=char3[i]+'A'-'a';
     }
     cout<<char3;
+
+}
+
+/*
+Se citeste un cuvant s format din maxim 200 de litere mici.
+Sa se elimine toate perechile de cate 2 litere alaturate identice.
+*/
+
+void problema4(){
+
+    char char4[200];
+    int i=0;
+    cin>>char4;
+    while(i<strlen(char4)-1) {
+        if(char4[i]==char4[i+1]) {
+            strcpy(char4+i,char4+i+2);
+            if(i>0) i=i-1;
+        }
+        else i++;
+    }
+    cout<<char4;
 
 }
 
